2c9e9daa7a8e820e1ad67a53e1fd465b295c51d7
ndpage <nathanpage2018@users.noreply.github.com>
2022-05-02 09:46:55 -0400

    Added standard python runable script layout

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import os
 import sys
 import trim.trim as trim
 
-# get input argument
-srcdir = str(sys.argv[1])
-print(f"Renaming in {srcdir}")
-
-for file in os.listdir(srcdir):
-  # trim timestamp from file name
-  dstfile = trim.file(file)  
-  # rename file
-  os.rename(f'{srcdir}/{file}',f'{srcdir}/{dstfile}')
-
-print(os.listdir(srcdir))
+def main():
+  # get input argument
+  srcdir = str(sys.argv[1])
+  print(f"Trimming file names in {srcdir}")
+  
+  for file in os.listdir(srcdir):
+    # trim timestamp from file name
+    dstfile = trim.file(file)  
+    # rename file
+    os.rename(f'{srcdir}/{file}',f'{srcdir}/{dstfile}')
+  
+  print(os.listdir(srcdir))
+  
+if __name__ == '__main__':
+  main()
